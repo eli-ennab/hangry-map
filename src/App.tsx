9bbd352d31cfe25d328c2dd40fe43233c2ca5dec
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 10:46:25 +0200

    added editorConfig and route set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import {Routes, Route} from 'react-router-dom'
+
 import './assets/App.scss'
 
-function App() {
+const App = () => {
   return (
-    <>
+	<Routes>
+				
+		{/* Guest Routes */}
+		<Route path="/" element={ <HomePage/> }/>
 
-    </>
+		<Route path="/login" element={<LoginPage/>}/>
+		<Route path="/logout" element={<LogoutPage/>}/>
+		<Route path="/signup" element={<SignupPage/>}/>
+
+		<Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+		<Route path="*" element={<NotFound/>}/>
+
+		
+		<Route path="/tips" element={}/>
+	
+		<Route path="/places" element={}/>
+		<Route path="/places/:id" element={}/>
+
+		{/* RequireAuth */}
+		<Route path="/user/:id" element={}/>
+		<Route path="/user/:id/create" element={}/>
+		<Route path="/user/:id/update-profile" element={}/>
+
+	</Routes>
+
   )
 }
 
