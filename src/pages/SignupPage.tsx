ca650e0b4cadcc2772a60a05d0a852235b8eff27
Eli Ennab <elinaennab@gmail.com>
2023-09-18 13:31:15 +0200

    fixed indentation and navigate user to /login when signed up successfully

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -13,7 +13,6 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 
 const SignupPage = () => {
-
 	const [loading, setLoading] = useState(false)
 	const [errorMessage, setErrorMessage] = useState<string | null>(null)
 	const {handleSubmit, register, watch, formState: {errors}} = useForm<SignUpCredentials>()
@@ -32,7 +31,7 @@ const SignupPage = () => {
 			
 			await signup(data.email, data.password)
 			
-			navigate('/')
+			navigate('/login')
 		} catch (error) {
 			if (error instanceof FirebaseError) {
 				setErrorMessage(error.message)
@@ -44,93 +43,93 @@ const SignupPage = () => {
 	}
 
   return  (
-	<Container className="py-3">
-		<Row>
-			<Col md={{span: 6, offset: 3}}>
-				<Card>
-					<Card.Body>
-						<Card.Title className="mb-3">Create new user</Card.Title>
-						{/* {errorMessage && {errorMessage}} */}
-										
-						<Form onSubmit={handleSubmit(onSignup)}>
-							<Form.Group controlId="email" className="mb-3">
-								<Form.Label>Email</Form.Label>
-								<Form.Control
-									placeholder="mail@example.com"
-									type="email"
-									{...register('email', {
-										required: 'E-Mail required'
-									})}
-								/>
-								{errors.email && 
-									<p>
-										{errors.email.message ?? "Invalid value"}
-									</p>
-								}
-							</Form.Group>
+		<Container className="py-3">
+			<Row>
+				<Col md={{span: 6, offset: 3}}>
+					<Card>
+						<Card.Body>
+							<Card.Title className="mb-3">Sign Up</Card.Title>
+							{/* {errorMessage && {errorMessage}} */}
+											
+							<Form onSubmit={handleSubmit(onSignup)}>
+								<Form.Group controlId="email" className="mb-3">
+									<Form.Label>Email</Form.Label>
+									<Form.Control
+										placeholder="mail@example.com"
+										type="email"
+										{...register('email', {
+											required: 'E-Mail required'
+										})}
+									/>
+									{errors.email && 
+										<p>
+											{errors.email.message ?? "Invalid value"}
+										</p>
+									}
+								</Form.Group>
+								
+								<Form.Group controlId="password" className="mb-3">
+									<Form.Label>Password</Form.Label>
+									<Form.Control
+										type="password"
+										{...register('password', {
+											required: "Password required",
+											minLength: {
+												value: 6,
+												message: "Please enter at least 6 characters"
+											}
+										})}
+									/>
+									{errors.password && 
+										<p>
+											{errors.password.message ?? "Invalid value"}
+										</p>
+									}
+									<Form.Text>You must have least 6 characters</Form.Text>
+								</Form.Group>
+								
+								<Form.Group controlId="confirmPassword" className="mb-3">
+									<Form.Label>Confirm Password</Form.Label>
+									<Form.Control
+										type="password"
+										autoComplete="off"
+										{...register('passwordConfirm', {
+											required: "Enter your password again",
+											minLength: {
+												value: 6,
+												message: "Please enter at least 6 characters"
+											},
+											validate: (value) => {
+												return value === passwordRef.current || "The passwords does not match"
+											}
+										})}
+									/>
+									{errors.passwordConfirm &&
+										<p>
+											{errors.passwordConfirm.message ?? "Invalid value"}
+										</p>
+									}
+								</Form.Group>
+								
+								<Button 
+									variant="dark" 
+									type="submit" 
+									disabled={loading}
+								>
+									Sign Up
+								</Button>
 							
-							<Form.Group controlId="password" className="mb-3">
-								<Form.Label>Password</Form.Label>
-								<Form.Control
-									type="password"
-									{...register('password', {
-										required: "Password required",
-										minLength: {
-											value: 6,
-											message: "Please enter at least 6 characters"
-										}
-									})}
-								/>
-								{errors.password && 
-									<p>
-										{errors.password.message ?? "Invalid value"}
-									</p>
-								}
-								<Form.Text>You must have least 6 characters</Form.Text>
-							</Form.Group>
-							
-							<Form.Group controlId="confirmPassword" className="mb-3">
-								<Form.Label>Confirm Password</Form.Label>
-								<Form.Control
-									type="password"
-									autoComplete="off"
-									{...register('passwordConfirm', {
-										required: "Enter your password again",
-										minLength: {
-											value: 6,
-											message: "Please enter at least 6 characters"
-										},
-										validate: (value) => {
-											return value === passwordRef.current || "The passwords does not match"
-										}
-									})}
-								/>
-								{errors.passwordConfirm &&
-									<p>
-										{errors.passwordConfirm.message ?? "Invalid value"}
-									</p>
-								}
-							</Form.Group>
-							
-							<Button 
-								variant="light" 
-								type="submit" 
-								disabled={loading}
-							>
-								Create User
-							</Button>
-						
-						</Form>
-					</Card.Body>
-				</Card>
-				
-				<div className="">
-					Already a user? <Link to="/login">Log In Here</Link>
-				</div>
-			</Col>
-		</Row>
-	</Container>
-  )
+							</Form>
+						</Card.Body>
+					</Card>
+					
+					<div className="">
+						Already a user? <Link to="/login">Log In Here</Link>
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	)
 }
 
 export default SignupPage
