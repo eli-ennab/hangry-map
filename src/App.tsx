d1ee42e598cc688e8be14e6f92edf31eb6b76cf9
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 10:59:13 +0200

    added navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
 import {Routes, Route} from 'react-router-dom'
-
+import Navigation from './pages/partials/Navigation'
 import './assets/App.scss'
 
 const App = () => {
   return (
-	<Routes>
-				
-		{/* Guest Routes */}
-		<Route path="/" element={ <HomePage/> }/>
+	<div id='app'>
 
-		<Route path="/login" element={<LoginPage/>}/>
-		<Route path="/logout" element={<LogoutPage/>}/>
-		<Route path="/signup" element={<SignupPage/>}/>
+		<Navigation/>
+		<Routes>
+					
+			{/* Guest Routes */}
+			<Route path="/" element={ <HomePage/> }/>
 
-		<Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
-		<Route path="*" element={<NotFound/>}/>
+			<Route path="/login" element={<LoginPage/>}/>
+			<Route path="/logout" element={<LogoutPage/>}/>
+			<Route path="/signup" element={<SignupPage/>}/>
 
+			<Route path="/forgot-password" element={<ForgotPasswordPage/>}/>
+			<Route path="*" element={<NotFound/>}/>
+
+			
+			<Route path="/tips" element={}/>
 		
-		<Route path="/tips" element={}/>
-	
-		<Route path="/places" element={}/>
-		<Route path="/places/:id" element={}/>
+			<Route path="/places" element={}/>
+			<Route path="/places/:id" element={}/>
 
-		{/* RequireAuth */}
-		<Route path="/user/:id" element={}/>
-		<Route path="/user/:id/create" element={}/>
-		<Route path="/user/:id/update-profile" element={}/>
+			{/* RequireAuth */}
+			<Route path="/user/:id" element={}/>
+			<Route path="/user/:id/create" element={}/>
+			<Route path="/user/:id/update-profile" element={}/>
 
-	</Routes>
+		</Routes>
+	</div>
 
   )
 }
---
+++ src/pages/partials/Navigation.tsx
@@ -0,0 +1,36 @@
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import {NavLink, Link} from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+	<Navbar bg="dark" variant="dark" expand="md">
+	<Container>
+		<Navbar.Brand as={Link} to="/">HangryMap</Navbar.Brand>
+		
+		<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+		<Navbar.Collapse id="basic-navbar-nav">
+			<Nav className="ms-auto">
+			
+						<Nav.Link as={NavLink} to="/tips">Tips</Nav.Link>
+						<Nav.Link as={NavLink} to="/places">Places</Nav.Link>
+
+					
+						<Nav.Link as={NavLink} to="/user/update-profile">Update Profil</Nav.Link>
+						
+						<Nav.Link as={NavLink} to="/user/create">Create Places</Nav.Link>
+
+				
+						<Nav.Link as={NavLink} to="/signup">Sign Up</Nav.Link>
+						<Nav.Link as={NavLink} to="/login">Log In</Nav.Link>
+						<Nav.Link as={NavLink} to="/logout">Log Out</Nav.Link>
+					
+			</Nav>
+		</Navbar.Collapse>
+	</Container>
+</Navbar>
+  )
+}
+
+export default Navigation
