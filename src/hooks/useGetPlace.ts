609cb9923216766da507b46e8b6d6aad6c9c2faf
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 21:51:04 +0200

    created custom hook for get a single place

---
+++ src/hooks/useGetPlace.ts
@@ -0,0 +1,9 @@
+import { placeCol } from '../services/firebase'
+import {Place} from '../types/Places.types.ts'
+import useStreamDocument from './useStreamDocument.ts'
+
+const useGetPlace = (placeId: string) => {
+	return useStreamDocument<Place>(placeCol, placeId)
+}
+
+export default useGetPlace
