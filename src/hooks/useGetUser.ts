9d8fe5137882763afefde5a2113862bd46d0fcaa
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 22:18:19 +0200

    created custom hook for get a single user

---
+++ src/hooks/useGetUser.ts
@@ -0,0 +1,9 @@
+import { userCol } from '../services/firebase'
+import {User} from '../types/User.types.ts'
+import useStreamDocument from './useStreamDocument.ts'
+
+const useGetUser = (userId: string) => {
+	return useStreamDocument<User>(userCol, userId)
+}
+
+export default useGetUser
