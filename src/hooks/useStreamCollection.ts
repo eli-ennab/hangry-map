70c17b61458c58515bac6b13108b5620e8d8be1b
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 21:12:30 +0200

    created a generic streamSnapshot for collections

---
+++ src/hooks/useStreamCollection.ts
@@ -0,0 +1,32 @@
+import {CollectionReference, onSnapshot, query, QueryConstraint} from 'firebase/firestore'
+import {useEffect, useState} from 'react'
+const useStreamCollection = <T>(
+	colRef: CollectionReference<T>,
+	...queryConstraints: QueryConstraint[]
+) => {
+	const [data, setData] = useState<T[]|null>(null)
+	const [loading, setLoading] = useState(true)
+	
+	
+	useEffect( () => {
+		const queryRef = query(colRef, ...queryConstraints)
+		return onSnapshot(queryRef, (snapshot) => {
+			const data: T[] = snapshot.docs.map(doc => {
+				return {
+					...doc.data(),
+					_id: doc.id,
+				}
+			})
+			setData(data)
+			setLoading(false)
+		})
+		
+	}, [colRef])
+	
+	return {
+		data,
+		loading,
+	}
+}
+
+export default useStreamCollection
