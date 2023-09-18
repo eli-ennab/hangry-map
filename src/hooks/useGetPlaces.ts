2aba0f3f282c6898db85777d1f6d7505586584a8
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-18 22:21:34 +0200

    changed hook to get all places regardless if they are approved or not

--- src/hooks/useGetPlaces.ts
+++ src/hooks/useGetPlaces.ts
@@ -1,4 +1,4 @@
-import {orderBy, where} from 'firebase/firestore'
+import {orderBy} from 'firebase/firestore'
 import useStreamCollection from './useStreamCollection.ts'
 import {placeCol,} from '../services/firebase.ts'
 import {Place} from '../types/Places.types.ts'
@@ -6,8 +6,7 @@ import {Place} from '../types/Places.types.ts'
 const useGetPlaces = () => {
 
     return useStreamCollection<Place>(placeCol, 
-		orderBy('name'),
-		where('isApproved', '==', true)
+		orderBy('name')
 	)
 }
     
