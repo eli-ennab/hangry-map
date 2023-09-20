1b2fd278b1f83f224ab81da6ec0f7075ac8120cd
Eli Ennab <elinaennab@gmail.com>
2023-09-20 15:36:53 +0200

    render admins from admin table

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,30 +1,34 @@
 import useAuth from "../hooks/useAuth"
 import PlacesTable from '../components/PlacesTable.tsx'
+import useGetAdmins from "../hooks/useGetAdmins.ts"
 import useGetPlaces from '../hooks/useGetPlaces.ts'
-// import useGetUser from "../hooks/useGetUser.ts"
 import useGetUsers from '../hooks/useGetUsers.ts'
 import { placesColumns } from '../tableSchema/Places.tsx'
 import { userColumns } from '../tableSchema/Users.tsx'
+import { adminColumns } from '../tableSchema/Admins.tsx'
 
 const DashboardPage = () => {
 	const { currentUser } = useAuth()
-	// const {data: user} = useGetUser(currentUser!.uid)
 	const {data: places} = useGetPlaces()
 	const {data: users} = useGetUsers()
-	
-	return currentUser && places && users ? (
+	const {data: admins} = useGetAdmins()
+
+	return currentUser && places && users && admins ? (
 			<>
-				
 				<PlacesTable 
 					columns={placesColumns} 	
 					data={places} 
 				/>
-				
+
 				<PlacesTable 
 					columns={userColumns} 
 					data={users} 
 				/>
 
+				<PlacesTable 
+					columns={adminColumns} 
+					data={admins} 
+				/>
 			</>
 		
 	) : null
