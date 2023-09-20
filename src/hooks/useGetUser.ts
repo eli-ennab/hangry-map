65d0faa6d8e949404f2b96cfde840fc3925ac8ce
Eli Ennab <elinaennab@gmail.com>
2023-09-20 11:57:15 +0200

    fixing spaces

--- src/hooks/useGetUser.ts
+++ src/hooks/useGetUser.ts
@@ -1,5 +1,5 @@
 import { userCol } from '../services/firebase'
-import {User} from '../types/User.types.ts'
+import { User } from '../types/User.types.ts'
 import useStreamDocument from './useStreamDocument.ts'
 
 const useGetUser = (userId: string) => {
