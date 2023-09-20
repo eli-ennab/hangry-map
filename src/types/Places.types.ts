506a31c89e8f5b4ab99a736cafee43bcc3f7f987
Eli Ennab <elinaennab@gmail.com>
2023-09-20 13:10:33 +0200

    fixed types cuz they were double

--- src/types/Places.types.ts
+++ src/types/Places.types.ts
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-export type Place = {
-	_id: string 
-	isApproved: boolean
-	name: string
-	address: string
-	city: string
-	description: string
-	category: PlaceCategory
-	offerings: PlaceOfferings[]
-	timestamp?: Date 
-	email?: string
-	phone?: string
-	website?: string
-	facebook?: string
-	instagram?: string
-	lat?: number 
-	lng?: number 
-}
-  
-export enum PlaceCategory {
-	Cafe = "Café",
-	Restaurant = "Restaurant",
-	FastFood = "Fastfood",
-	KioskGrill = "Kiosk/Grill",
-	FoodTruck = "Foodtruck"
-}
-  
-export enum PlaceOfferings {
-	Lunch = "Lunch",
-	AfterWork = "After Work",
-	Dinner = "Dinner/Á la carte"
-}
-=======
 import { Timestamp } from "firebase/firestore"
 
 export type Place = {
@@ -68,5 +34,4 @@ export enum PlaceOfferings {
 	AfterWork = "After Work",
 	Dinner = "Dinner/Á la carte"
 }
->>>>>>> dev
   
