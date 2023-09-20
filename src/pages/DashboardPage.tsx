47477815bfeb627713cf9e833fcb0dbe9282e8ff
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-20 21:06:34 +0200

    added badge for each tab, user and admin show how many there is of each, places show how many that is approved of all places

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -8,13 +8,23 @@ import { userColumns } from '../tableSchema/Users.tsx'
 import { adminColumns } from '../tableSchema/Admins.tsx'
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import {Badge} from 'react-bootstrap'
 
 const DashboardPage = () => {
 	const { currentUser } = useAuth()
 	const {data: places} = useGetPlaces()
 	const {data: users} = useGetUsers()
 	const {data: admins} = useGetAdmins()
-
+	
+	// const approvedPlaces = <Badge bg="success" text={'dark'}>{places?.filter(p => p.isApproved) .length}</Badge>
+	// const allPlaces = <Badge bg="danger" className={''} text={'dark'}>{places?.length}</Badge>
+	
+	
+	const placesBadge = <span className={'d-block'}> <Badge bg="success" text={'dark'}>{places?.filter(p => p.isApproved) .length}</Badge> of <Badge bg="danger" className={''} text={'dark'}>{places?.length}</Badge> approved </span>
+	
+	const adminsCount = <span className={'d-block'}>  <Badge bg="primary" text={'dark'}>{admins?.length}</Badge></span>
+	const userCount = <span className={'d-block'}>  <Badge bg="warning" text={'dark'}>{users?.length}</Badge></span>
+	
 	return currentUser && places && users && admins ? (
 			<>
 				<Tabs
@@ -22,19 +32,19 @@ const DashboardPage = () => {
 					className="table-tabs mb-3"
 					fill
 				>
-					<Tab eventKey="places" title="Places">
+					<Tab eventKey="places" title={!placesBadge  ? 'Places' : <> Places {placesBadge}</>}>
 						<PlacesTable 
 							columns={placesColumns} 	
 							data={places} 
 						/>
 					</Tab>
-					<Tab eventKey="users" title="Users">
+					<Tab eventKey="users" title={!userCount || users.length === 0 ? 'Users' : <>Users {userCount}</>}>
 						<PlacesTable 
 							columns={userColumns} 
 							data={users} 
 						/>
 					</Tab>
-					<Tab eventKey="admins" title="Admins">
+					<Tab eventKey="admins" title={!adminsCount || admins.length === 0 ? 'Admins' : <>Admins {adminsCount}</>}>
 						<PlacesTable 
 							columns={adminColumns} 
 							data={admins} 
