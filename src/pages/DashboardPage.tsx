018865929a810e98b08d664f19d32d2dd5fcfa6b
Eli Ennab <elinaennab@gmail.com>
2023-09-20 15:31:25 +0200

    moving users table to component + indentation

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,54 +1,29 @@
-import { createColumnHelper } from '@tanstack/react-table'
 import useAuth from "../hooks/useAuth"
-
 import PlacesTable from '../components/PlacesTable.tsx'
 import useGetPlaces from '../hooks/useGetPlaces.ts'
-import useGetUser from "../hooks/useGetUser.ts"
+// import useGetUser from "../hooks/useGetUser.ts"
 import useGetUsers from '../hooks/useGetUsers.ts'
-
-import { User } from '../types/User.types.ts'
-import Image from 'react-bootstrap/Image'
-
-
-import {placesColumns} from '../tableSchema/Places.tsx'
+import { placesColumns } from '../tableSchema/Places.tsx'
+import { userColumns } from '../tableSchema/Users.tsx'
 
 const DashboardPage = () => {
 	const { currentUser } = useAuth()
-	const {data: user} = useGetUser(currentUser!.uid)
+	// const {data: user} = useGetUser(currentUser!.uid)
 	const {data: places} = useGetPlaces()
 	const {data: users} = useGetUsers()
 	
-
-	const columnUsersHelper = createColumnHelper<User>()
-	
-	const userColumns = [
-		columnUsersHelper.group({
-			header: 'Users',
-			columns: [
-				
-				columnUsersHelper.accessor('photoURL', {
-					header: 'IMG',
-					cell: props => (
-						<Image className={'w-25'} src={props.getValue()} />
-					)
-				}),
-				columnUsersHelper.accessor('name', {
-					header: 'Name',
-				}),
-				columnUsersHelper.accessor('email', {
-					header: 'E-Mail',
-				}),
-				columnUsersHelper.accessor('admin', {
-					header: 'Admin',
-				}),
-			],
-		}),
-	]
 	return currentUser && places && users ? (
 			<>
 				
-			<PlacesTable columns={placesColumns} data={places} />
-			<PlacesTable columns={userColumns} data={users} />
+				<PlacesTable 
+					columns={placesColumns} 	
+					data={places} 
+				/>
+				
+				<PlacesTable 
+					columns={userColumns} 
+					data={users} 
+				/>
 
 			</>
 		
--- src/tableSchema/Places.tsx
+++ src/tableSchema/Places.tsx
@@ -1,9 +1,8 @@
-import {Link} from 'react-router-dom'
-import {createColumnHelper} from '@tanstack/react-table'
-import {Place} from '../types/Places.types.ts'
+import { Link } from 'react-router-dom'
+import { createColumnHelper } from '@tanstack/react-table'
+import { Place } from '../types/Places.types.ts'
 
 const columnPlaceHelper = createColumnHelper<Place>()
-
 
 export const placesColumns = [
 	columnPlaceHelper.group({
