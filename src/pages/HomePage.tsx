a4c854d607d51eb9116a4d97ac31a0ae1233364d
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-23 20:23:23 +0200

    added error and fetching position state and error handling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,51 +1,61 @@
-
-import React, { useEffect, useMemo, useState } from "react"
+import React, {useEffect, useMemo, useState} from "react"
 import AutoComplete from "../components/AutoComplete"
 import useGetPlacesApproved from "../hooks/useGetPlacesApproved"
 import PlaceDetailsOffCanvas from "../components/PlaceDetailsOffCanvas"
 import PlacesOffCanvas from "../components/PlacesOffCanvas"
 import {LatLngLiteral, Place} from "../types/Places.types"
-import { Button } from "react-bootstrap"
+import {Button, Spinner} from "react-bootstrap"
 import Map from '../components/Map.tsx'
+import Alert from 'react-bootstrap/Alert'
 
 
 const HomePage = () => {
-
+	
+	const [fetchPos, setFetchPos] = useState(false)
+	const [error, setError] = useState(false)
+	const [errorMsg, setErrorMsg] = useState('')
+	
+	
 	const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
 	const [showPlacesCanvas, setShowPlacesCanvas] = useState(false)
+	
 	const [userPos, setUserPos] = useState<LatLngLiteral>({
-		lat: 55.8604395,
-		lng: 55.8604395,
+		lat: 55.60500288357208,
+		lng: 13.00195277298942
 	})
 	const [zoom, setZoom] = useState(9)
-
-	useEffect(() => {		
+	
+	useEffect(() => {
+		setError(false)
 		if (navigator.geolocation) {
+			setFetchPos(true)
 			navigator.geolocation.getCurrentPosition((position) => {
 				const newPos: LatLngLiteral = {
 					lat: position.coords.latitude,
 					lng: position.coords.longitude,
-				};
+				}
 				
-				setZoom(14)
+				setZoom(16)
 				setUserPos(newPos)
-				
+				setFetchPos(false)
 			}, (error) => {
-				console.error("Error getting location:", error)
+				setErrorMsg(`Error getting location: ${error.message}. Try reloading and accept "Use your location" to get the best experience!  `)
+				setFetchPos(false)
+				setError(true)
 			})
 		} else {
-			console.log("Geolocation not supported")
+			setErrorMsg("Geolocation not supported")
+			setFetchPos(false)
+			setError(true)
 		}
 	}, [])
+	
 	return (
-		<>
-
-			
-			
-			<Map userPos={userPos} zoom={zoom} />
-
-
-			
+			<>
+				{error && <Alert variant="danger" className={'text-center mt-3 w-75 mx-auto'}>{errorMsg}</Alert>}
+				{fetchPos && <Alert variant={'dark'} className={'text-center mt-3 w-75 mx-auto'}>Fetching your position...</Alert>}
+				
+				<Map userPos={userPos} zoom={zoom}/>
 				
 				{/*<Button onClick={() => setShowPlacesCanvas(true)}>All places in your area</Button>*/}
 				
@@ -60,7 +70,7 @@ const HomePage = () => {
 				{/*	onHide={() => setShowPlacesCanvas(false)}*/}
 				{/*/>*/}
 			
-		</>
+			</>
 	)
 }
 
