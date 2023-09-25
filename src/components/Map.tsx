c2bd24ae3b317fd6fe52558597aeb2f635af18e1
nilssoncjonas <nilssoncjonas@gmail.com>
2023-09-25 14:04:05 +0200

    added distance calc

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -86,16 +86,14 @@ const [dist, setDist] = useState(0)
 			lat: PLlat,
 			lng: PLlng 
 		}
-		
-		const R = 6378137; // Earth’s mean radius in meter
-		const dLat = rad(p2.lat - p1.lat);
-		const dLong = rad(p2.lng - p1.lng);
+		const R = 6378137
+		const dLat = rad(p2.lat - p1.lat)
+		const dLong = rad(p2.lng - p1.lng)
 		const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
 				Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) *
-				Math.sin(dLong / 2) * Math.sin(dLong / 2);
-		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		const d = R * c;
-		// return d; // returns the distance in meter
+				Math.sin(dLong / 2) * Math.sin(dLong / 2)
+		const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+		const d = R * c
 		setDist(d)
 	}
 	
