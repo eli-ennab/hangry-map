e0f1fe54ef97fdc0351c921d00f20a674243247e
Eli Ennab <elinaennab@gmail.com>
2023-09-25 11:32:16 +0200

    offcanvas header and body, listgroup and items, style and sort alphabetically

--- src/components/PlacesOffCanvas.tsx
+++ src/components/PlacesOffCanvas.tsx
@@ -1,4 +1,5 @@
-import { Offcanvas } from 'react-bootstrap'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Offcanvas from 'react-bootstrap/Offcanvas'
 import { Place } from '../types/Places.types'
 
 type PlacesOffCanvasProps = {
@@ -8,20 +9,28 @@ type PlacesOffCanvasProps = {
 }
 
 const PlacesOffCanvas: React.FC<PlacesOffCanvasProps> = ({ places, show, onHide }) => {
+	const sortedPlaces = places?.slice().sort((a, b) => a.name.localeCompare(b.name))
 
 	return (
-		<Offcanvas show={show} onHide={onHide} placement="start" title="All Places">
+		<Offcanvas show={show} onHide={onHide} placement="start" title="Places near you">
+				<Offcanvas.Header className="placeListHeader" closeButton>
+					<Offcanvas.Title>
+						Places near you
+					</Offcanvas.Title>
+				</Offcanvas.Header>
 			<Offcanvas.Body>
-				{places?.map(place => (
-					<div key={place._id} className="mb-3">
-						<strong>{place.name}</strong><br />
-						{place.address}<br />
-						<hr />
-					</div>
-				))}
+				<ListGroup>
+					{sortedPlaces?.map(place => (
+						<ListGroup.Item key={place._id} className="mb-3 placeListItem">
+							<strong>{place.name}</strong>
+							<br />
+							{place.address}
+						</ListGroup.Item>
+					))}
+				</ListGroup>
 			</Offcanvas.Body>
 		</Offcanvas>
 	)
 }
 
-export default PlacesOffCanvas;
+export default PlacesOffCanvas
